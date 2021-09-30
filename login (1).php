4d1a7d201713896af1ce450aef0d1e51f711735e
rokkocevar <64775667+rokkocevar@users.noreply.github.com>
2021-09-30 08:24:13 +0200

    Update login (1).php

--- login (1).php
+++ login (1).php
@@ -12,9 +12,9 @@
         ?>
         <div id="body">
         <h2>Prijava</h2>
-        <form action="login_check.php" method="POST">
-            <input type="email" name="ema" placeholder="Vnesite email" class="form-control"><br>
-            <input type="geslo" name="ges" placeholder="Vnesite geslo" class="form-control"><br>
+        <form action="login_check.php" method="post">
+            <input type="email" name="em" placeholder="Vnesite email" class="form-control"><br>
+            <input type="geslo" name="ge" placeholder="Vnesite geslo" class="form-control"><br>
             <br>
             <a href="register.php">Še nimate računa? Registrirajte se.</a><br>
             <button type="submit" class="btn btn-primary">Prijavi se!</button>
