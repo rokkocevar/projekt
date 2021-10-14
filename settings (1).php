18931a9de9bd6313f7176f5ab9e4583df525a12c
rokkocevar <64775667+rokkocevar@users.noreply.github.com>
2021-10-14 08:21:34 +0200

    Add files via upload

---
+++ settings (1).php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '767811450053-2ft6kn4ant9htq4pddoqs2a2rrkdgc7i.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', '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');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'https://www.vozila.kocevar.xyz/login_check.php');
+
+?>
