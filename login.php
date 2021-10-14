5d5d2773517817749a4fb94b6746a7eb20f03ee9
rokkocevar <64775667+rokkocevar@users.noreply.github.com>
2021-10-14 08:17:24 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html >
-    <head>
+    <html >
+       
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+           
     </head>
     <body>
         
         <?php
         include 'glava.php';
+        require_once('settings.php');
+
+$login_url = 'https://accounts.google.com/o/oauth2/v2/auth?scope=' . urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email') . '&redirect_uri=' . urlencode(CLIENT_REDIRECT_URL) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online';
         ?>
        
+   
        
         <div id="body">
         <h2>Prijava</h2>
@@ -19,50 +25,14 @@
             <input type="email" name="em" placeholder="Vnesite email" class="form-control"><br>
             <input type="password" name="ge" placeholder="Vnesite geslo" class="form-control"><br>
             <br>
+            
             <a href="register.php">Še nimate računa? Registrirajte se.</a><br>
             <button type="submit" class="btn btn-primary">Prijavi se!</button>
+            <a href="<?= $login_url ?>">Login with Google</a>
         </form>
-        <?php
         
-       include 'autoload.php';
-                         $clientID='767811450053-2ft6kn4ant9htq4pddoqs2a2rrkdgc7i.apps.googleusercontent.com';
-                         $clientSecret='AIzaSyAUJS_CpnCrFeF78P293Cp5ff9e0UrlOY8';
-                         $redirectUrl = 'https://www.vozila.kocevar.xyz/login.php';
-                         
-                         $client = new Google_Client();
-                         
-                         $client->setClientId($clientID);
-                         
-                         $client->setClientSecret($clientSecret);
-                         $client->setRedirectUri($redirectUrl);
-                         $client->addScope('profile');
-                         $client->addScope('email');
-                        
-                          if(isset($_GET['code'])){
-                             $client->authenticate($_GET['code']);
-                             $access_token = $client->getAccessToken();
-                                
-                              $gauth = new Google_Service_Oauth2($client);
-                              $google_info = $gauth->userinfo->get();
-                              $email = $google_info->email;
-                              $name = $google_info->name;
-                              $password = $google_info->password;
-                              $_SESSION['namu'] = $google_info->name;
-                             
-               
-                             header("Location: index.php");
-                         
-                        
-                            
-                        }
-                        else{
-                         echo "<a href='".$client->createAuthUrl()."' class='btn-google m-b-10'>
-                                    <img src='prenos.png' alt='GOOGLE'>
-                                    Google
-                                    </a>";
-                        }
 
-                    ?>
+
         </div>
         <?php
         require 'noga.php';
